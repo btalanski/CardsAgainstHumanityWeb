9a73e5e0bc662e311b25ad09e0d5872b9086b559
Bruno Talanski <btalanski@lixar.com>
2019-10-21 10:33:45 -0300

    Initial commit

---
+++ data/bCards.js
@@ -0,0 +1,57 @@
+export const bCards = [
+    {
+        value: "Resolução da ANAC agora proíbe [#] em aeronaves.",
+        draw: 0,
+        pick: 1
+    },
+    {
+        value: "É uma pena que as crianças de hoje em dia estejam todas envolvidas em [#].",
+        draw: 0,
+        pick: 1,
+    },
+    {
+        value: "Em 1000 anos, quando o dinheiro de papel for apenas uma lembrança distante, [#] será nossa moeda",
+        draw: 0,
+        pick: 1,
+    },
+    {
+        value: "CBF proíbe [#] por proporcionar vantagem desleal aos jogadores",
+        draw: 0,
+        pick: 1,
+    },
+    {
+        value: "Qual é o prazer proibido do Batman ?",
+        draw: 0,
+        pick: 1,
+    },
+    {
+        value: "Vem ai o novo livro de J. K. Rowling: Harry Potter  e a Câmara de [#].",
+        draw: 0,
+        pick: 1,
+    },
+    {
+        value: "Desculpa, professora, mas é que eu não pude terminar o dever de casa por causa de [#]",
+        draw: 0,
+        pick: 1,
+    },
+    {
+        value: "Nunca de fato entendi [#], até encontrar [#]",
+        draw: 0,
+        pick: 2,
+    },
+    {
+        value: "Globo filmes apresenta [#], a história de [#]",
+        draw: 0,
+        pick: 2,
+    },
+    {
+        value: "Sim, eu matei [#]. Como ? [#]",
+        draw: 0,
+        pick: 2,
+    },
+    {
+        value: "[#] + [#] = [#].",
+        draw: 2,
+        pick: 3,
+    },
+];
---
+++ data/wCards.js
@@ -0,0 +1,140 @@
+export const wCards = [
+    {
+        value: "Uma maldição cigana.",
+    },
+    {
+        value: "Um momento de silêncio.",
+    },
+    {
+        value: "Um festival da linguiça.",
+    },
+    {
+        value: "Um policial honesto sem nada a perder.",
+    },
+    {
+        value: "Escassez de comida.",
+    },
+    {
+        value: "Bactéria carnívora.",
+    },
+    {
+        value: "Erva para aumentar a libido",
+    },
+    {
+        value: "Não dar a mínima para o Terceiro Mundo.",
+    },
+    {
+        value: "Sexting.",
+    },
+    {
+        value: "Transmutação corpórea.",
+    },
+    {
+        value: "Atrizes pornô.",
+    },
+    {
+        value: "Estuprar e saquear.",
+    },
+    {
+        value: "72 virgens.",
+    },
+    {
+        value: "Fuzilamento",
+    },
+    {
+        value: "Um paradoxo de viagem no tempo",
+    },
+    {
+        value: "Autêntica cozinha mexicana.",
+    },
+    {
+        value: "Cintilante",
+    },
+    {
+        value: "Consultores",
+    },
+    {
+        value: "Dívida monstruosa.",
+    },
+    {
+        value: "Problemas com o pai na infância.",
+    },
+    {
+        value: "Cotas raciais.",
+    },
+    {
+        value: "Deixar o lustre cair sobre seus inimigos enquanto a outra ponta da corda te lança para cima.",
+    },
+    {
+        value: "O ex-presidente George W. Bush.",
+    },
+    {
+        value: "Nudez frontal.",
+    },
+    {
+        value: "Injeções hormonais.",
+    },
+    {
+        value: "Pôr um ovo.",
+    },
+    {
+        value: "Assistir Cartoon Network pelado(a).",
+    },
+    {
+        value: "Fingir que se importa.",
+    },
+    {
+        value: "Pagar mico em público.",
+    },
+    {
+        value: "Compartilhar agulhas.",
+    },
+    {
+        value: "Meleca de nariz.",
+    },
+    {
+        value: "A inevitável morte térmica do universo.",
+    },
+    {
+        value: "O milagre do parto.",
+    },
+    {
+        value: "O apocalipse.",
+    },
+    {
+        value: "Passar o rodo.",
+    },
+    {
+        value: "Uma viagem para Pelotas.",
+    },
+    {
+        value: "Desodorante AXE.",
+    },
+    {
+        value: "O sangue de Cristo.",
+    },
+    {
+        value: "Horríveis acidentes causados por depilação a laser.",
+    },
+    {
+        value: "BATMAN!!!",
+    },
+    {
+        value: "Agricultura.",
+    },
+    {
+        value: "Um mongolóide vigoroso.",
+    },
+    {
+        value: "Seleção natural.",
+    },
+    {
+        value: "Aborto clandestino",
+    },
+    {
+        value: "Comer todos os doces que deveriam ser vendidos na campanha de caridade.",
+    },
+    {
+        value: "Os braços da Michelle Obama",
+    },
+];
