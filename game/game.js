8a5dc082e6d6d09bb70f88433320bf7bca0c6e02
Bruno Talanski <btalanski@lixar.com>
2019-10-21 16:07:46 -0300

    Set default states for game

--- game/game.js
+++ game/game.js
@@ -1,3 +1,32 @@
+const defaultState = {
+    players: [],
+    gameName: "",
+    matchStarted: false,
+    maxAllowedPlayers: 6,
+    defaultTurnTimeoutSeconds: 60,
+    defaultTimeoutSeconds: 5,
+    maxNumberOfRounds: null,
+    roundsCount: null,
+    questionDeck: [],
+    answerDeck: [],
+    selectedCard: null,
+    enableChat: true,
+}
+
+const defaultPlayerState = {
+    nickName: null,
+    isOwner: false,
+    color: "#000000",
+    portrait: null,
+    isRoundLeader: false,
+    cards: [],
+    selectedCards: [],
+};
+
+const defaultChatState = {
+    chatLog:[],
+}
+
 const gameLogic = (io) => {
     io.on('connection', function (socket) {
         console.log('a user connected');
